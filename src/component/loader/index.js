d5c8be9352775a3027aa1903d448229e52cae712
liem-nguyenhoang <liem.nguyenhoang.127@gmail.com>
2020-11-11 16:23:36 +0700

    Create Loader 011627

--- src/component/loader/index.js
+++ src/component/loader/index.js
@@ -1,15 +1,58 @@
-// import React,{useContext} from 'react'
-// import { ActivityIndicator, Text,StyleSheet,Dimensions
-// ,Platform } from 'react-native'
-// import { color } from "../../utility";
-// import {  } from "../../context/";
+import React, { useContext } from 'react'
+import {
+    ActivityIndicator,
+    Text,
+    StyleSheet,
+    Dimensions,
+    Platform,
+    View,
+} from 'react-native'
+import { color } from "../../utility";
+import { Store } from "../../context/store";
 
-// const Loader = () => {
-//     return (
-//         <View>
-//             <Text></Text>
-//         </View>
-//     )
-// }
+const { height, width } = Dimensions.get("window");
 
-// export default Loader
+const Loader = () => {
+    // const globalState = useContext(Store);
+    // const { mapLoaderState } = globalState;
+    // const { loading } = mapLoaderState;
+    let loading= true;
+    return loading ? (
+        <View style={styles.loaderContainer}>
+            <View style={styles.indicator}>
+                <ActivityIndicator
+                    size="large"
+                    animating={loading}
+                    color={color.WHITE}
+                    style={{
+                        left: Platform.OS === 'ios' ? 1.3 : 0,
+                        right: Platform.OS === 'ios' ? 1 : 0,
+                    }} x />
+            </View>
+        </View>
+    ) : null;
+};
+
+const styles = StyleSheet.create({
+    loaderContainer: {
+        zIndex: 1,
+        elevation: 2,
+        height,
+        width,
+        position: 'absolute',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: color.SEMI_TRANSPARENT,
+    },
+    indicator: {
+        backgroundColor: color.DARK_GRAY,
+        height: 44,
+        width: 44,
+        borderRadius: 22,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignContent: 'center',
+    },
+});
+export default Loader;
